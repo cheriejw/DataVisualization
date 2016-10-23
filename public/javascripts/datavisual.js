9cf3e54085bef2003da5537e8108c87528a1065e
Cherie Woo <cheriejawoo@yahoo.com>
2016-10-23 06:15:38 -0700

    Gunna kill someone...

--- public/javascripts/datavisual.js
+++ public/javascripts/datavisual.js
@@ -69,6 +69,7 @@ var unit = (middle/4); //theyre all this many spaces appart; one unit
 var rad = unit/3;
 var ymid = (window.innerHeight/3);
 var syndec = middle + unit;
+var yunit = ymid/4;
 
 for(syno in syn){
     syndec = syndec - unit;
@@ -110,38 +111,43 @@ var svgContainer = d3.select("body").append("div")
 
 var node = svgContainer.selectAll(".node")
   .data(jsonCircles)
-  .enter()
-  .append("g")
+  .enter().append("g")
+  .attr("class", "node")
+  .attr("transform", function(d) { return "translate (" + d.x + "," + d.y + ")";})
   .on('click', function(d){
                             //console.log(d.word); //gives me the location
-                            var x = d.x_axis;
-                            console.log(x);
-                            d3.selectAll("g").transition() //FUCK YEAH!
+                            var tem = d.x_axis;
+                            // console.log(x);
+                            d3.selectAll("text", "circle").transition() //FUCK YEAH!
                                 .duration(1500)
                                 .delay(100) //wait a while before you move
                                 // .attr("cx", function (d) { var x = d.x_axis; d.x_axis = d.x_axis + (x - middle); return d.x_axis; }) //this expands
-                                .attr("cx", function (d) { d.x_axis = d.x_axis - (x - middle); return d.x_axis; })
+                                .attr("x", function (d) { d.x_axis = (d.x_axis - (tem - middle)); return d.x_axis; });
                                 // .attr("cx", shiftx(d))
                                 //.transition()
                                 //.attr("cy", function (d) { return d.y_axis + unit; })
                                 //console.log(d.x_axis);
+                            d3.selectAll("circle").transition()
+                                .duration(1500)
+                                .delay(100)
+                                .attr("cx", function (d) { d.x_axis = d.x_axis - (tem - middle); return d.x_axis; });
                         });
 
 node.append("text")
                   .attr("x", function (d) {return d.x_axis;})
-                  .attr("y", function (d) {return d.y_axis;})
+                  .attr("y", function (d) {return d.y_axis + yunit;})
                   .text( function(d) { return d.word; })
                   //.text("test")
                   //console.log(this.text)
                   .attr("font-family", "sans-serif")
-                  .attr("font-size", "40px")
+                  .attr("font-size", "30px")
                   .attr("fill", "white");
 
 
 node.append("circle")
 .attr("cx", function (d) { return d.x_axis; })
-                        //.attr("cx", xaxis(d)) ; 
-                        .attr("cy", function (d, i, nodes) { return d.y_axis;})
+                        .attr("x", function (d) { return d.x_axis;}) 
+                        .attr("cy", function (d) { return d.y_axis;})
                         .attr("r", function (d) { return d.radius; })
                         //.attr("w", function (d) { return d.word; })
                         //.text(function(d) { return d.word; })
