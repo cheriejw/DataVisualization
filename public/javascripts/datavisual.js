ed1ceb2b58a538fc54d258f0ab30b8904cade310
Cherie Woo <cheriejawoo@yahoo.com>
2016-10-22 19:12:44 -0700

    Starting to understand D3...
    but not really...

--- public/javascripts/datavisual.js
+++ public/javascripts/datavisual.js
@@ -1,4 +1,4 @@
-var words = ["Saab", "Volvo", "BMW"]; //will be replaced with values.
+var words = ["Saab", "Volvo", "BMW", "Updated"]; //will be replaced with values.
 // var roots = 
 // var syn = 
 // var ant = 
@@ -9,13 +9,29 @@ for(word in words){ //for a word in words
 }
 
 var jsonCircles = [ //the box it produces in is 0-200 units.
-  { "x_axis": 0, "y_axis": 100, "radius": 20, "color" : "green" },
+  { "x_axis": 20, "y_axis": 100, "radius": 20, "color" : "green" },
   { "x_axis": 70, "y_axis": 100, "radius": 20, "color" : "purple"},
   { "x_axis": 200, "y_axis": 100, "radius": 20, "color" : "red"}]; //this is an array of objects
 
+//break
+// var scrollCircle = d3.select(jsonCircles)
+// 						.transition()
+
+// d3.selectAll("circles").transition()
+//     .delay(function(d, i) { return i * 50; })
+//     .on("click", function repeat() {
+//         d3.active(this)
+//             .style("fill", "red")
+//           .transition()
+//             .style("fill", "green")
+//           .transition()
+//             .style("fill", "blue")
+//           .transition()
+//             .on("start", repeat);
+//       });
 //make an svg container
 var svgContainer = d3.select("body").append("div")
-						.classed("svg-container", true)
+						.classed("svg-container", true) //classed means css
 						.append("svg")
                         .attr("preserveAspectRatio", "xMinYMin meet") //responsive, but tiny circles too.
                         .attr("viewBox", "0 0 600 400")
@@ -31,4 +47,12 @@ var circleAttributes = circles
                         .attr("cx", function (d) { return d.x_axis; })
                         .attr("cy", function (d) { return d.y_axis; })
                         .attr("r", function (d) { return d.radius; })
-                        .style("fill", function(d) { return d.color; });
+                        .style("fill", function(d) { return d.color; })
+                        //performing transitions
+                        .transition()
+                        .duration(1500)
+                        .delay(1000) //wait a while before you move
+                        .attr("cx", "100")
+                        .transition()
+                        .attr("cy", "130");
+ 
